c409ce2b915ae7bd73d566f4998a207159975fb0
AlexandrosKap <alexandroskapretsos@gmail.com>
2022-12-18 18:00:59 +0200

    animations have keyframes

--- src/momo/animation.nim
+++ src/momo/animation.nim
@@ -1,3 +1,4 @@
+from strformat import `&`
 import easing
 
 # TODO: WORKING ON IT UWU
@@ -7,38 +8,59 @@ type
     value*: T
     time*: float
     easingFunc*: EasingFunc
-  Animation* = ref object # working on it...
+  Animation*[T] = ref object
+    keyframes*: seq[Keyframe[T]]
+    time*: float
 
 func keyframe*[T](value: T, time: float, easingFunc: EasingFunc): Keyframe[T] =
-  Keyframe(
+  Keyframe[T](
     value: value,
     time: if time < 0.0: 0.0 else: time,
     easingFunc: easingFunc
   )
 
-func keyframe*[T](value: T, easingFunc: EasingFunc): Keyframe[T] =
-  Keyframe(
+func keyframe*[T](value: T, time: float): Keyframe[T] =
+  Keyframe[T](
     value: value,
-    time: 0.0,
-    easingFunc: easingFunc
-  )
-
-func keyframe*[T](value: T): Keyframe[T] =
-  Keyframe(
-    value: value,
-    time: 0.0,
+    time: time,
     easingFunc: linear
   )
 
-func ease*[T](first: Keyframe[T], last: Keyframe[T], time: float): T =
-  if time < first.time: first.value
+func ease*[T](self: Keyframe[T], last: Keyframe[T], time: float): T =
+  if time < self.time: self.value
   elif time > last.time: last.value
-  elif last.time < first.time: last.value
+  elif last.time < self.time: last.value
   else:
     ease(
-      first.easingFunc,
-      first.value,
+      self.easingFunc,
+      self.value,
       last.value,
-      time - first.time,
-      last.time - first.time
+      time - self.time,
+      last.time - self.time
     )
+
+func `$`*[T](self: Keyframe[T]): string =
+  &"keyframe({self.value}, {self.time})"
+
+func newAnimation*[T](keyframes: varargs[Keyframe[T]]): Animation[T] =
+  result = Animation[T]()
+  for keyframe in keyframes:
+    result.keyframes.add(keyframe)
+
+func duration*(self: Animation): float =
+  if self.keyframes.len != 0:
+    self.keyframes[^1].time
+  else:
+    0.0
+
+func advance*(self: Animation, time: float) =
+  self.time += time
+  if self.time > self.duration:
+    self.time = self.duration
+
+func `$`*(self: Animation): string =
+  result = ""
+  for i, keyframe in self.keyframes:
+    result.add(&"{i}: {keyframe}")
+    if i != self.keyframes.len - 1:
+      result.add('\n')
